58ca48576d222a4badcc903f482cedef5befbcbf
sebchiarelli <47152370+sebchiarelli@users.noreply.github.com>
2020-08-24 17:43:51 +0200

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,5 +15,5 @@ function vanishing2(){
     container[1].classList.add("vanish");
 }
 function vanishing3(){
-    document.querySelector("body").classList.toggle("vanish");
+    //document.querySelector("body").classList.toggle("vanish");
 }
